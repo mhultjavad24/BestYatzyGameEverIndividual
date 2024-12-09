e92205a223f21a6008740c814cab2448cf491002
Melina Skytén <melina.skyten@gmail.com>
2024-12-09 13:49:09 +0100

    Added rules in properties file, and methods to read the file.

--- src/Game.java
+++ src/Game.java
@@ -9,6 +9,7 @@ public class Game {
     public static void main(String[] args) {
 
 
+
     }
 
 }
--- src/Rules.java
+++ src/Rules.java
@@ -1,2 +1,30 @@
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class Rules {
+
+    private String rules;
+
+    public Rules() {
+
+        Properties properties = new Properties();
+
+        try {
+            properties.load(new FileInputStream("src/Rules.properties"));
+        } catch (Exception e) {
+            System.out.println("Rules file could not be loaded");
+            e.printStackTrace();
+        }
+
+        setRules(properties.getProperty("rules"));
+    }
+
+    public String getRules() {
+        return "Rules: " + rules;
+    }
+
+    private void setRules(String rules) {
+        this.rules = rules;
+    }
+
 }
