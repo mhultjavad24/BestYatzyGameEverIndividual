258e0d640928f7c5c2591ea960b422d24f6649fe
Joakim Skäppegård <joakim_skappegard@hotmail.com>
2024-12-10 23:16:20 +0100

    Tror jag täckt upp allt vi pratade om och mer där till.
    Med detta sagt har jag säkert missat de viktigaste mest grundläggande sakerna vi behöver...
    
    Tror att vi bör göra ett urval från den här branchen och lägga in i main.
    
    Har inte testat allt, så det finns betydlig risk för buggar/missar.

--- src/Dice.java
+++ src/Dice.java
@@ -1,5 +1,6 @@
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.Random;
@@ -10,7 +11,7 @@ public class Dice {
     private int sides;
 
     public Dice(int sides) {
-        pips = 1;
+        this.pips = 1;               //Kanske bör vara 0 för att visa att tärningen inte blivit rullad
         this.sides = sides;
     }
 
@@ -20,12 +21,16 @@ public class Dice {
     }
 
     public static List<Dice> getDice(int amountOfDice){
-
+        List<Dice> dice = new ArrayList<Dice>();
+        for(int i = 0; i < amountOfDice; i++){
+            dice.add(new Dice());
+        }
+        return dice;
     }
 
     public void rollDice() {
         Random rng = new Random();
-        this.pips = rng.nextInt(6) + 1;
+        this.pips = rng.nextInt(sides) + 1;
     }
 
     public void rollXTimes(int times) {
@@ -33,6 +38,18 @@ public class Dice {
         for (int i = 0; i < times; i++) {
             rollDice();
             System.out.println(""+pips);
+        }
+    }
+
+    public void reRollAll(List<Dice> dice) {
+        for (Dice die : dice) {
+            die.rollDice();
+        }
+    }
+
+    public void reRollSome(List<Dice> dice, List<Integer> selectedDice) {
+        for (Integer i : selectedDice) {
+            dice.get(selectedDice.get(i)).rollDice();
         }
     }
 
@@ -71,4 +88,7 @@ public class Dice {
         this.sides = Integer.parseInt(properties.getProperty("sides", "6"));
     }
 
+    public int getPips() {
+        return pips;
+    }
 }
