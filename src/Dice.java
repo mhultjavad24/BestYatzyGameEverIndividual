42ce391097338b03fdb2dca2248086554f50cda7
Joakim Skäppegård <joakim_skappegard@hotmail.com>
2024-12-10 14:30:54 +0100

    comit innan paus för att hämnta sonen från förskola.
    lagt till properties
    
    fått metod för att visa tärningsansikten att fundera.
    
    metod för att läsa properties
    
    ...

--- src/Dice.java
+++ src/Dice.java
@@ -1,3 +1,7 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
 import java.util.Random;
 
 public class Dice {
@@ -10,25 +14,61 @@ public class Dice {
         this.sides = sides;
     }
 
+    public Dice(){
+        loadProperties();
+        rollDice();
+    }
+
+    public static List<Dice> getDice(int amountOfDice){
+
+    }
+
     public void rollDice() {
         Random rng = new Random();
         this.pips = rng.nextInt(6) + 1;
     }
 
-    public void showDieFace() {
-        if (pips == 1){
-            System.out.println("\\u{2680}");
-        }else if (pips == 2){
-            System.out.println("\\u{2681}");
-        }else if (pips == 3){
-            System.out.println("\\u{2682}");
-        }else if (pips == 4){
-            System.out.println("\\u{2683}");
-        }else if (pips == 5){
-            System.out.println("\\u{2684}");
-        }else if (pips == 6){
-            System.out.println("\\u{2685}");
+    public void rollXTimes(int times) {
+        Random rng = new Random();
+        for (int i = 0; i < times; i++) {
+            rollDice();
+            System.out.println(""+pips);
         }
+    }
+
+    public void showDieFace() { //detta är unicode 16.0 jaga vesrion 22 stöder upp till 15 om jag förstår det rätt
+        switch (this.pips){
+            case 1:
+                System.out.println("⚀");
+                break;
+            case 2:
+                System.out.println("⚁");
+                break;
+            case 3:
+                System.out.println("⚂");
+                break;
+            case 4:
+                System.out.println("⚃");
+                break;
+            case 5:
+                System.out.println("⚄");
+                break;
+            case 6:
+                System.out.println("⚅");
+                break;
+        }
+    }
+
+    public void loadProperties(){
+        Properties properties = new Properties();
+        try{
+            properties.load(new FileInputStream("src/Dice.properties"));
+        }
+        catch (IOException e){
+            e.printStackTrace();
+            System.out.println("Kunde inte läsa properties");
+        }
+        this.sides = Integer.parseInt(properties.getProperty("sides", "6"));
     }
 
 }
--- src/Game.java
+++ src/Game.java
@@ -8,6 +8,12 @@ public class Game {
 
     public static void main(String[] args) {
 
+        Dice die1 = new Dice(6);
+
+        die1.showDieFace();
+
+        die1.rollXTimes(100);
+
 
     }
 
