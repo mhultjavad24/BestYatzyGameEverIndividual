a8beb3b5284c3f0b9c0d45f5c1f661b3c0ea5cc0
Joakim Skäppegård <joakim_skappegard@hotmail.com>
2024-12-11 13:36:57 +0100

    Initial commit med bortkomenterad kod för att ta bort inför merge.

--- src/Dice.java
+++ src/Dice.java
@@ -10,10 +10,10 @@ public class Dice {
     private int pips;
     private int sides;
 
-    public Dice(int sides) {
-        this.pips = 1;               //Kanske bör vara 0 för att visa att tärningen inte blivit rullad
-        this.sides = sides;
-    }
+//    public Dice(int sides) {
+//        this.pips = 1;               //Kanske bör vara 0 för att visa att tärningen inte blivit rullad
+//        this.sides = sides;
+//    }
 
     public Dice(){
         loadProperties();
@@ -33,13 +33,13 @@ public class Dice {
         this.pips = rng.nextInt(sides) + 1;
     }
 
-    public void rollXTimes(int times) {
-        Random rng = new Random();
-        for (int i = 0; i < times; i++) {
-            rollDice();
-            System.out.println(""+pips);
-        }
-    }
+//    public void rollXTimes(int times) {
+//        Random rng = new Random();
+//        for (int i = 0; i < times; i++) {
+//            rollDice();
+//            System.out.println(""+pips);
+//        }
+//    }
 
     public void reRollAll(List<Dice> dice) {
         for (Dice die : dice) {
@@ -47,34 +47,34 @@ public class Dice {
         }
     }
 
-    public void reRollSome(List<Dice> dice, List<Integer> selectedDice) {
-        for (Integer i : selectedDice) {
-            dice.get(selectedDice.get(i)).rollDice();
-        }
-    }
+//    public void reRollSome(List<Dice> dice, List<Integer> selectedDice) {
+//        for (Integer i : selectedDice) {
+//            dice.get(selectedDice.get(i)).rollDice();
+//        }
+//    }
 
-    public void showDieFace() { //detta är unicode 16.0 jaga vesrion 22 stöder upp till 15 om jag förstår det rätt
-        switch (this.pips){
-            case 1:
-                System.out.println("⚀");
-                break;
-            case 2:
-                System.out.println("⚁");
-                break;
-            case 3:
-                System.out.println("⚂");
-                break;
-            case 4:
-                System.out.println("⚃");
-                break;
-            case 5:
-                System.out.println("⚄");
-                break;
-            case 6:
-                System.out.println("⚅");
-                break;
-        }
-    }
+//    public void showDieFace() { //detta är unicode 16.0 jaga vesrion 22 stöder upp till 15 om jag förstår det rätt
+//        switch (this.pips){
+//            case 1:
+//                System.out.println("⚀");
+//                break;
+//            case 2:
+//                System.out.println("⚁");
+//                break;
+//            case 3:
+//                System.out.println("⚂");
+//                break;
+//            case 4:
+//                System.out.println("⚃");
+//                break;
+//            case 5:
+//                System.out.println("⚄");
+//                break;
+//            case 6:
+//                System.out.println("⚅");
+//                break;
+//        }
+//    }
 
     public void loadProperties(){
         Properties properties = new Properties();
