ae53696f94f6360dc0af7331366a61336de25012
Joakim Skäppegård <joakim_skappegard@hotmail.com>
2024-12-11 13:38:16 +0100

    Dice prepade inför merge

--- src/Game.java
+++ src/Game.java
@@ -8,13 +8,6 @@ public class Game {
 
     public static void main(String[] args) {
 
-        Dice die1 = new Dice(6);
-
-        die1.showDieFace();
-
-        die1.rollXTimes(100);
-
-
     }
 
 }
